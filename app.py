f808789fb54677492787165dd64f61677883a1cf
yogasukrasena <yogawayan4@gmail.com>
2019-10-15 04:24:31 +0800

    ketiga

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ from datetime import date
 app = Flask(__name__)
 PORT = int(os.environ.get("PORT", 5000))
 
+connection = pymysql.connect(host='db4free.net',
+                             user='yogasukrasena',
+                             password='Ikadek07',
+                             db='db_paten_yoga',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
 # create a route for webhook
 @app.route('/', methods=['POST'])
 def webhook():
@@ -22,13 +29,46 @@ def webhook():
     return jsonify(data)
 
 def Webhook(data):
-    pesan = data.get("queryResult").get("queryText")
+    cekUserID = data.get("originalDetectIntentRequest").get("payload").get("data").get("from").get("id")
+    id_pesan = data.get("originalDetectIntentRequest").get("payload").get("message_id")
+    pesan = data.get("originalDetectIntentRequest").get("payload").get("text")
+    id_inbox = ""
 
-    response = {
-        'fulfillmentText': pesan
-    }
+    try:
+        result = None
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM tb_ WHERE tb_profile.userID = %s"
+            cursor.execute(sql, (cekUserID))
+            result = cursor.fetchone()
 
-    return jsonify(response)
+        response = {
+            'fulfillmentMessages': [
+                {
+                    "card": {
+                        "title": "Menu",
+                        "subtitle": "Halo {}, Silahkan pilih menu di bawah".format(result['nama']),
+                        "buttons": [
+                            {
+                                "text": "Cek Profil",
+                                "postback": "cek profil"
+                            },
+                            {
+                                "text": "Info Akademik",
+                                "postback": "info akademik"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        return jsonify(response)
+
+    except Exception:
+        response = {
+            'fulfillmentText': "Akun anda belum terkait dengan Sistem Simak, mohon input nim Anda"
+        }
+        return jsonify(response)
+
 
 # run the app
 if __name__ == '__main__':
