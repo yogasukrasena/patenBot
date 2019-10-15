10b6133a5799006ba549d7ab57096ffcd6ebeb06
yogasukrasena <yogawayan4@gmail.com>
2019-10-16 00:13:25 +0800

    keenam

--- app.py
+++ app.py
@@ -21,39 +21,36 @@ connection = pymysql.connect(host='db4free.net',
 def webhook():
     data = request.get_json()
     intent_name = data.get("queryResult").get("intent").get("displayName")
+    print(data)
+
+    if intent_name == 'webhook-intent':
+        return Awal(data)
+
+    return jsonify(data)
+
+def Awal(data):
     cekUserID = data.get("originalDetectIntentRequest").get("payload").get("from").get("id")
     idPesan = data.get("originalDetectIntentRequest").get("payload").get("message_id")
-    isiPesan = data.get("originalDetectIntentRequest").get("payload").get("text")
-    print(data)
+    pesan = data.get("originalDetectIntentRequest").get("payload").get("text")
+    tanggal = data.get("originalDetectIntentRequest").get("payload").get("date")
+    id_inbox = ""
 
     try:
         result = ""
         with connection.cursor() as cursor:
-            sql = "INSERT INTO tb_inbox (id_pesan, pesan, userID, tanggal) VALUES (%d, %s, %s, %s)"
-            cursor.execute(sql, (idPesan, isiPesan, cekUserID, date.today().strftime("%Y-%m-%d")))
-            # idterakhir = cursor.lastrowid
+            sql = "INSERT INTO tb_inbox (id_pesan, pesan, userID, tanggal) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (idPesan, pesan, cekUserID, date.today().strftime("%Y-%m-%d")))
+            id_inbox = cursor.lastrowid
             # sql = "INSERT INTO tb_outbox (id_inbox, pesan, date) VALUES (%s, %s, %s)"
             # cursor.execute(sql, (idterakhir, order(data), date.today().strftime("%Y-%m-%d")))
-        connection.commit()
-    except Exception:
-        response = {
-            'fulfillmentText': "Akun anda belum terkait dengan Sistem Simak, mohon input nim Anda"
-        }
-        return jsonify(response)
-    finally:
-        connection.close()
-
-    if intent_name == 'webhook-intent':
-        return Webhook(data)
-
-def Webhook(data):
+            connection.commit()
 
         response = {
             'fulfillmentMessages': [
                 {
                     "card": {
                         "title": "Menu",
-                        "subtitle": "Halo {}, Silahkan pilih menu di bawah",
+                        "subtitle": "Halo {}, Silahkan pilih menu di bawah".format(result['nama']),
                         "buttons": [
                             {
                                 "text": "Cek Profil",
@@ -70,11 +67,11 @@ def Webhook(data):
         }
         return response
 
-    # except Exception:
-    #     response = {
-    #         'fulfillmentText': "Akun anda belum terkait dengan Sistem Simak, mohon input nim Anda"
-    #     }
-    #     return jsonify(response)
+    except Exception:
+        response = {
+            'fulfillmentText': "Akun anda belum terkait dengan Sistem Simak, mohon input nim Anda"
+        }
+        return jsonify(response)
 
 
 # run the app
