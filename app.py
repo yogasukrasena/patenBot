5f9472d6fe187f0eb6278c93edbc2a737253266c
yogasukrasena <yogawayan4@gmail.com>
2019-10-16 15:11:05 +0800

    kekude

--- app.py
+++ app.py
@@ -32,6 +32,10 @@ def webhook():
     elif intent_name == 'menu.pengajuan.dagang':
         return perdagangan()
 
+    elif intent_name == 'surat_pengantar_dagang - nama - no.tlpn':
+        return dataUserPengaju(data)
+
+
     return jsonify(request.get_json())
 
 def Awal(data):
@@ -124,8 +128,8 @@ def perdagangan():
         'fulfillmentMessages': [
             {
                 "card": {
-                    "title": "Menu",
-                    "subtitle": "Silahkan pilih menu di bawah",
+                    "title": "SIUP",
+                    "subtitle": "Silahkan pilih layanan di Bawah",
                     "buttons": [
                         {
                             "text": "Form Perdagangan",
@@ -141,6 +145,50 @@ def perdagangan():
         ]
     }
     return response
+
+def dataUserPengaju(data):
+    cekUserID = data.get("originalDetectIntentRequest").get("payload").get("from").get("id")
+    idPesan = data.get("originalDetectIntentRequest").get("payload").get("message_id")
+    isiPesan = data.get("originalDetectIntentRequest").get("payload").get("text")
+    id_inbox = ""
+
+    try:
+        result = ""
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO tb_inbox (id_pesan, pesan, userID, tanggal) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (idPesan, isiPesan, cekUserID, date.today().strftime("%Y-%m-%d")))
+            # id_inbox = cursor.lastrowid
+            result = cursor.fetchone()
+        connection.commit()
+
+        response = {
+            'fulfillmentMessages': [
+                {
+                    "card": {
+                        "title": "Menu",
+                        "subtitle": "Halo {}, Silahkan pilih menu di bawah".format(isiPesan),
+                        "buttons": [
+                            {
+                                "text": "Surat izin Usaha Perdagangan",
+                                "postback": "usaha perdagangan"
+                            },
+                            {
+                                "text": "Pengajuan Izin Reklame",
+                                "postback": "izin reklame"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        return response
+
+    except Exception:
+        response = {
+            'fulfillmentText': "Data anda gagal di Daftarkan"
+        }
+        return jsonify(response)
+
 
 
 # run the app
