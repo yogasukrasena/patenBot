94dcaaf1ea37fdbea8cb54217c4817efd6a5d66a
yogasukrasena <yogawayan4@gmail.com>
2019-10-16 20:21:11 +0800

    kekude

--- app.py
+++ app.py
@@ -165,29 +165,29 @@ def dataUserPengaju(data):
     jenisusaha = outputContexts[parameter_index]['parameters']['jenisusaha']
     notlpnusaha = outputContexts[parameter_index]['parameters']['notlpnusaha']
 
-    print("{} {} {} {} {} {} {} {}".format(namauser, nikuser, alamat, notlpn, namausaha, alamatusaha, jenisusaha, notlpnusaha))
+    try:
+        result = ""
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO tb_user_pengaju (id_user, nik, nama_user, alamat, no_tlp, tanggal) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, (cekUserID, nikuser, namauser, alamat, notlpn, date.today().strftime("%Y-%m-%d")))
+            id_inbox = cursor.lastrowid
+            sql2 = "INSERT INTO tb_user_pengaju_detail (id_user_pengaju, nama_usaha, alamat_usaha, jenis_usaha, no_tlpn_usaha) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql2, (id_inbox, namausaha, alamatusaha, jenisusaha, notlpnusaha,))
+            result = cursor.fetchone()
+        connection.commit()
 
-    # try:
-    result = ""
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO tb_user_pengaju (id_user, nik, nama_user, alamat, no_tlp, tanggal) VALUES (%s, %s, %s, %s, %s)"
-        cursor.execute(sql, (cekUserID, nikuser, namauser, alamat, notlpn, date.today().strftime("%Y-%m-%d")))
-        id_inbox = cursor.lastrowid
-        sql2 = "INSERT INTO tb_user_pengaju_detail (id_user_pengaju, nama_usaha, alamat_usaha, jenis_usaha, no_tlpn_usaha) VALUES (%s, %s, %s, %s, %s)"
-        cursor.execute(sql2, (id_inbox, namausaha, alamatusaha, jenisusaha, notlpnusaha,))
-        result = cursor.fetchone()
-    connection.commit()
+        response = {
+            'fulfillmentText': "Selamat, data anda berhasil di masukan"
+        }
+        return jsonify(response)
 
-    response = {
-        'fulfillmentText': "Selamat, data anda berhasil di masukan"
-    }
-    return jsonify(response)
+    except Exception as error:
+        print(error)
 
-    # except Exception:
-    #     response = {
-    #         'fulfillmentText': "Data anda gagal di Daftarkan"
-    #     }
-    #     return jsonify(response)
+        response = {
+            'fulfillmentText': "Data anda gagal di Daftarkan"
+        }
+        return jsonify(response)
 
 
 
